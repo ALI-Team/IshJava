e7f812f2bb85c9fcd4027692091b02ff2331fa70
IvarMatstoms <ivar.matstoms@gmail.com>
2016-08-20 01:31:22 +0200

    removed shit to reduce text game

--- IshJava/src/testgame/Ball.java
+++ IshJava/src/testgame/Ball.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package testgame;
 
 import IshJava.Game;
 import IshJava.GameObject;
 
-/**
- *
- * @author Ivar
- */
 public class Ball extends GameObject{
     
     public Ball(Game game, int x, int y) {
@@ -19,10 +11,8 @@ public class Ball extends GameObject{
         this.setSprite("src/imgs/ball.png");
         moveto(600, 200,200);
     }
-    
     @Override
     public void tick(){
         
-    } 
-    
+    }  
 }
--- IshJava/src/testgame/TestGameMain.java
+++ IshJava/src/testgame/TestGameMain.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package testgame;
 
 import IshJava.Game;
 import IshJava.Main;
 import IshJava.GameObject;
-/**
- *
- * @author ivamat907
- */
+
 public class TestGameMain {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
             Game game=Main.createGame(512,512, "test game");
             Ball ball=new Ball(game,0,0);
