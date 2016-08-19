7a3022a98f1eacb9a224a1c5681ddf8b5dfcb814
IvarMatstoms <ivar.matstoms@gmail.com>
2016-08-19 14:43:00 +0200

    Added the fucking files

---
+++ IshJava/src/IshJava/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package IshJava;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author ivamat907
+ */
+public class Main {
+    public static void createGame(){
+        JFrame frame = new JFrame();
+        JPanel jpanel = new JPanel();
+        frame.setVisible(true);
+        frame.add(jpanel); 
+    }
+    
+}
---
+++ IshJava/src/testgame/TestGameMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testgame;
+
+import IshJava.Main;
+
+/**
+ *
+ * @author ivamat907
+ */
+public class TestGameMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Main.createGame();
+        // TODO code application logic here
+    }
+    
+}
