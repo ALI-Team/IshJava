447b9954aaea3113be00e69ef3c905101c9b2070
AxelMatstoms <axemat060@edu.linkoping.se>
2016-09-07 13:45:30 +0200

    Fixed tabs in testgame and made Main abstract

--- IshJava/src/testgame/TestGameMain.java
+++ IshJava/src/testgame/TestGameMain.java
@@ -13,31 +13,31 @@ import java.awt.Color;
 public class TestGameMain {
 
     public static void main(String[] args) {
-            Game game=Main.createGame(512,512, "test game");
-            Ball ball=new Ball(game,200,200);
-            game.addObject(ball);
-            game.addObject(new boy(game,0,0,ball));   
-            
-            UITextView tw = new UITextView(16, 64, "hello world");
-            tw.setColor(new Color(0xFFFFFF));
-            tw.setFontFamily(UITextView.IMPACT);
-            tw.setFontSize(36);
-            tw.drawOutline(Color.BLACK);
-            tw.drawBackground(Color.GREEN);
-            tw.setPadding(8);
-            tw.setBorderRadius(16);
-            tw.drawBorder(Color.CYAN);
-            tw.setBorderWidth(5);
-            game.addUIElement(tw);
-            
-            MyButton button = new MyButton(16, 256);
-            button.setText("Press me");
-            button.drawBackground(Color.RED);
-            button.setFontSize(28);
-            button.setFontFamily(UITextButton.MONOSPACE);
-            button.setPadding(16);
-            button.setBorderRadius(4);
-            game.addUIElement(button);
+        Game game=Main.createGame(512,512, "test game");
+        Ball ball=new Ball(game,200,200);
+        game.addObject(ball);
+        game.addObject(new boy(game,0,0,ball));   
+        
+        UITextView tw = new UITextView(16, 64, "hello world");
+        tw.setColor(new Color(0xFFFFFF));
+        tw.setFontFamily(UITextView.IMPACT);
+        tw.setFontSize(36);
+        tw.drawOutline(Color.BLACK);
+        tw.drawBackground(Color.GREEN);
+        tw.setPadding(8);
+        tw.setBorderRadius(16);
+        tw.drawBorder(Color.CYAN);
+        tw.setBorderWidth(5);
+        game.addUIElement(tw);
+        
+        MyButton button = new MyButton(16, 256);
+        button.setText("Press me");
+        button.drawBackground(Color.RED);
+        button.setFontSize(28);
+        button.setFontFamily(UITextButton.MONOSPACE);
+        button.setPadding(16);
+        button.setBorderRadius(4);
+        game.addUIElement(button);
     }
     
 }
