16d37adef0e3f2772953d3dc4c8e8831f1d33df3
ivar.matstoms <a002123@c21081.ad.smhi.se>
2016-10-17 14:07:37 +0200

    rotate fix

--- IshJava/src/IshJava/GameObject.java
+++ IshJava/src/IshJava/GameObject.java
@@ -41,7 +41,7 @@ import javax.sound.sampled.*;
 public class GameObject extends GamePoint {
 
     /*
-    * @author ALI-Team  
+    * @author ALI-Team
      */
     public Image sprite;
     public boolean solid;
@@ -59,7 +59,7 @@ public class GameObject extends GamePoint {
     public int layer=0;
     public boolean rotate=false;
     public double rotation;
-    
+
     public char[] movementKeys;
     //detectHit
     public boolean collide(GameObject obj) {
@@ -84,13 +84,13 @@ public class GameObject extends GamePoint {
         speed = game.pps2ppf(v);
 
     }
-    
+
     public void rotate(int deg){
         rotate=true;
         this.rotation=Math.toRadians(deg);
     }
 
-    
+
     public void addKeyMovment(char up,char down,char left,char right,int speed){
         movementKeys=new char[4];
         movementKeys[0]=right;
@@ -100,7 +100,7 @@ public class GameObject extends GamePoint {
         setSpeed(speed);
         game.objectManager.keyMovementListeners.add(this);
     }
-    
+
     public GameObject(Game g, double x, double y) {
         this.game = g;
         movementmode = 0;
@@ -160,34 +160,34 @@ public class GameObject extends GamePoint {
     }
 
     public void moveobj() {
-        
+
         int oldX = (int) this.x;
         int oldY = (int) this.y;
         if (pen) {
-            
+
                 //if (canvas == null) {
                 //    canvas = new BufferedImage(game.width, game.height, BufferedImage.TYPE_INT_ARGB);
                 //}
-            
+
                 Graphics2D g2 = game.canvas.createGraphics();
                 g2.setStroke(this.penStroke);
                 g2.setPaint(this.penColor);
 
-                g2.drawLine((int)oldX + this.width / 2, (int)oldY + this.height / 2, (int)this.x + this.width / 2, (int)this.y + this.height / 2);            
-                
+                g2.drawLine((int)oldX + this.width / 2, (int)oldY + this.height / 2, (int)this.x + this.width / 2, (int)this.y + this.height / 2);
+
                 }
         if (movementmode == 1) {
             double dir = this.angleTo(target);
             double movelen = Math.min(speed, this.distance(target));
             this.x += movelen * Math.cos(dir);
             this.y += movelen * Math.sin(dir);
-            
-            
+
+
         } else if (movementmode == 2) {
             this.x += speed * Math.cos(direction);
             this.y += speed * Math.sin(direction);
-            
-         
+
+
         }
     }
 
@@ -208,17 +208,18 @@ public class GameObject extends GamePoint {
         if (canvas != null) {
             //g.drawImage(canvas, 0, 0, null);
         }
-        
+
         if (sprite != null) {
             if(!rotate){
             g.drawImage(sprite, (int) x, (int) y, null);
             }else{
-                Graphics2D g2d=(Graphics2D)g;
-                AffineTransform transform = g2d.getTransform();
-                g2d.translate(width/2, height/2);
-                g2d.rotate(rotation);
-                g.drawImage(sprite, (int) x, (int) y, null);
-                g2d.setTransform(transform);
+              //TACK NIKLAS FÖR KODEN XXXDDD
+              AffineTransform xform = new AffineTransform();
+                xform.translate((int) (x+0.5*width), (int) (y+0.5*height));
+                xform.rotate(this.rotation);
+                xform.translate(-width / 2, -height / 2);
+                ((Graphics2D) g).drawImage(sprite, xform, null);
+
                         }
         }
         this.g = g;
@@ -226,11 +227,11 @@ public class GameObject extends GamePoint {
 
     //Also to be overwritten. Called when mouse clicks the object
     public void onClick() {
-        
+
     }
 
     public synchronized void playSound(final String path) {
-        
+
         /*
          * Play sound on separate thread
          */
@@ -256,7 +257,7 @@ public class GameObject extends GamePoint {
             }
         }).start();
     }
-    
+
     public void clearPen() {
         if (canvas != null) {
             Graphics2D g2 = canvas.createGraphics();
